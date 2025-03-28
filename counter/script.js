e0ff8b4f5b76b349c763ac1be92a5b5b0934beea
kamaljeet <kamaljeet8130@gmail.com>
2025-03-28 14:57:36 +0530

    counter functionality added

--- counter/script.js
+++ counter/script.js
@@ -4,22 +4,24 @@ const counterValue = document.querySelector('.counter-value');
 btns.forEach((btn) => {
   btn.addEventListener('click', (event) => {
     let count = Number(counterValue.textContent);
+
     if (event.currentTarget.classList.contains('reset')) {
       count = 0;
-      counterValue.classList.remove('decrease', 'increase');
-      counterValue.textContent = count;
     } else if (event.currentTarget.classList.contains('decrease')) {
-      count -= 1;
-      counterValue.textContent = count;
-      if (count < 0) {
-        counterValue.classList.add('decrease');
-      }
+      count--;
     } else if (event.currentTarget.classList.contains('increase')) {
-      count += 1;
-      counterValue.textContent = count;
-      if (count > 0) {
-        counterValue.classList.add('increase');
-      }
+      count++;
+    }
+    counterValue.textContent = count;
+
+    counterValue.classList.remove('decrease', 'increase', 'reset');
+
+    if (count > 0) {
+      counterValue.classList.add('increase');
+    } else if (count < 0) {
+      counterValue.classList.add('decrease');
+    } else {
+      counterValue.classList.add('reset');
     }
   });
 });
