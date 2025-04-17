0e446926ee52e82cd6e424a90c1499a6667cc7fb
kamaljeet <kamaljeet8130@gmail.com>
2025-04-18 01:22:19 +0530

    js script for oops based counter added

--- counter-using-oops/script.js
+++ counter-using-oops/script.js
@@ -4,7 +4,36 @@ function getElement(selection) {
     return element;
   } else {
     throw new Error(
-      `please Check "${selection}" selector, no such element exists`
+      `please check ${selection} selector, no such element exists`
     );
   }
 }
+
+function Counter(element, value) {
+  this.counter = element;
+  this.value = value;
+  this.resetBtn = element.querySelector('.reset');
+  this.increaseBtn = element.querySelector('.increase');
+  this.decreaseBtn = element.querySelector('.decrease');
+  this.valueDOM = element.querySelector('.value');
+  this.valueDOM.textContent = this.value;
+  this.increaseBtn.addEventListener('click', this.increase.bind(this));
+  this.decreaseBtn.addEventListener('click', this.decrease.bind(this));
+  this.resetBtn.addEventListener('click', this.reset.bind(this));
+}
+
+Counter.prototype.increase = function () {
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.reset = function () {
+  this.value = 0;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.decrease = function () {
+  this.value--;
+  this.valueDOM.textContent = this.value;
+};
+
+const firstCounter = new Counter(getElement('.first-counter'), 230);
+const secondCounter = new Counter(getElement('.second-counter'), 343);
