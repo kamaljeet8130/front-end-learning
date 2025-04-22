b0bb19a19e2da2cb2d12bbbd0d245b366637594f
kamaljeet <kamaljeet8130@gmail.com>
2025-04-22 22:58:46 +0530

    added pre loader for video player

--- video-player/script.js
+++ video-player/script.js
@@ -10,3 +10,8 @@ btn.addEventListener('click', function () {
     video.play();
   }
 });
+
+const preloader = document.querySelector('.preloader');
+window.addEventListener('load', function () {
+  preloader.classList.add('hide-preloader');
+});
